e7e6c72207a8bb87569623099592103af2d4fcf4
stefanos <stefanosengl@gmail.com>
2020-03-20 11:20:38 +0100

    routes fixed soon

--- src/api/routers/halal-router.js
+++ src/api/routers/halal-router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('<h2>ALOOO</h2>')
+    res.send('alo')
 })
 
 module.exports = router
