131904fc6dfa26d14cb5940cddfc907082fb96f8
stefanos <stefanosengl@gmail.com>
2020-03-20 12:25:31 +0100

    deployment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express() 
-const port = 3000
+const port = process.env.PORT || 5000
 const databaseConnector = require('./database/db-connector')
 const fileUpload = require('express-fileupload') 
 
